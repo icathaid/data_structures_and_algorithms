74dccb95933bae3a4c0a3cdc1842ea10c99b332f
James McDaniel <icathaid@gmail.com>
2018-10-11 23:14:45 -0700

    fixed eslint issue

--- test/array_binary_search.test.js
+++ test/array_binary_search.test.js
@@ -11,15 +11,15 @@ describe('meatball test', () => {
   });
 
   it('can return the correct array position of a valid search', () => {
-      let arr = [1,2,3,4,5,6,7,8,9];
-      let target = 6;
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let target = 6;
     expect(binarySearch(arr, target)).toEqual(5);
   });
 
   it('returns -1 if no match is found', () => {
-    let arr = [1,2,3,4,5,6,7,8,9];
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let target = 15;
     expect(binarySearch(arr, target)).toEqual(-1);
   });
-  
+
 });
