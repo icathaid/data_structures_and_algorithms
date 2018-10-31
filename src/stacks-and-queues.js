959ee54ac7f171891e8e558f0fa7fe784c311b0c
James McDaniel <icathaid@gmail.com>
2018-10-30 20:32:31 -0700

    peek function works

--- src/stacks-and-queues.js
+++ src/stacks-and-queues.js
@@ -29,35 +29,47 @@ class Stack {
   }
   pop(){
     //  add error checking for being called on empty list
+    if(!this.head){
+      return console.log('you called pop on an empty list');
+    }
     let current = this.head;
     while(current.next.next){
       current = current.next;
     }
     let deletedNode = current.next;
     current.next = null;
-    console.log('deleted node', deletedNode);
-    console.log('FIX ME       ', bill.top);
     this.top = current;
-    console.log('FIXED ME       ', bill.top);
     return deletedNode;
+  }
+  peek(){
+    return this.top;
   }
 }
 
-const bill = new Stack;
+// const bill = new Stack;
 
-bill.push(1).push(2).push(3).push(4);
+// bill.push(1).push(2).push(3).push(4);
 
-console.log('-=-=-=-=-PRE POP BILL-=-=-=-=-');
-console.log(bill.head);
-console.log(bill.head.next);
-console.log(bill.head.next.next);
-console.log(bill.head.next.next.next);
+// console.log('-=-=-=-=-PRE POP BILL-=-=-=-=-');
+// console.log(bill.head);
+// console.log(bill.head.next);
+// console.log(bill.head.next.next);
+// console.log(bill.head.next.next.next);
 
-bill.pop();
+// bill.pop();
 
-console.log('-=-=-=-=-POST POP BILL-=-=-=-=-');
-console.log(bill.head);
-console.log(bill.head.next);
-console.log(bill.head.next.next);
-console.log(bill.head.next.next.next);
-console.log('post pop bill.top:     ', bill.top);
+// console.log('-=-=-=-=-POST POP BILL-=-=-=-=-');
+// console.log(bill.head);
+// console.log(bill.head.next);
+// console.log(bill.head.next.next);
+// console.log(bill.head.next.next.next);
+// console.log('post pop bill.top:     ', bill.top);
+
+// const ted = new Stack;
+
+// ted.push(9).push(8).push(7);
+// console.log('ted.top:    ', ted.top);
+// console.log('ted.peek', ted.peek());
+
+// const rufus = new Stack;
+// console.log('rufus', rufus.peek());
