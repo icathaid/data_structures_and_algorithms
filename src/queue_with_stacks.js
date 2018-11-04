0c249e42061176df77d42a8bc4e66aa23013d4f3
James McDaniel <icathaid@gmail.com>
2018-11-04 14:18:01 -0800

    committing unknown changes

---
+++ src/queue_with_stacks.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { Stack } = require('./stacks-and-queues.js');
+const Node = require('./node.js');
+
+class PseudoQueue {
+  constructor(){
+    stackA.head = 
+  }
+  enqueue(val){
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
